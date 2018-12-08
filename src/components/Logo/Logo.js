64f7144321dcaa319d55459a6ea92d6cf49d12ab
Martin Gorupić <37804840+MARTIN111Zg@users.noreply.github.com>
2018-12-08 10:01:17 +0100

    Create Logo.js

---
+++ src/components/Logo/Logo.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import Tilt from 'react-tilt';
+import brain from './brain.png';
+import './Logo.css';
+
+
+const Logo=()=>{
+return (
+<div className='ma4 mt0'>
+<Tilt className="Tilt" options={{ max : 55 ,scale:1.2,perspective:300}} style={{ height: 150, width: 150 }} >
+ <div className="Tilt-inner">
+<img src={brain} alt='logo'/>
+ </div>
+</Tilt>
+</div>
+);
+}
+
+export default Logo;
